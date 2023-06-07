14b6677edab1037ca8710e6ceb89ae9169dd8cf2
runrli <runrli@tencent.com>
2023-06-07 15:04:16 +0800

    改下conf.py

--- source/conf.py
+++ source/conf.py
@@ -23,9 +23,6 @@ language = 'zh_CN'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-import sphinx_rtd_theme
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
 html_theme = 'sphinx_rtd_theme'
 extensions = ['recommonmark','sphinx_markdown_tables']
 html_static_path = ['_static']
